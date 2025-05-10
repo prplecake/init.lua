642bceb7169d4c1212671f11b26c8b9e50407fdf
Matty Jorgensen <me@prplecake.com>
2025-05-10 17:49:13 -0500

    initial commit

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"williamboman/mason.nvim"
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig"
+		}
+	}
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            sync_install = false,
+            highliight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+    }
+}
